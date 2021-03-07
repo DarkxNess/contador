a2b2a01e5ea5cdbc8ccee96be5e567fb33bd7055
mquezada-at-081096265314 <hawking2009@hotmail.es>
2021-03-07 19:34:19 -0300

    [CSS] new responsives and collapse css

--- components/Header.vue
+++ components/Header.vue
@@ -1,47 +1,52 @@
 <template>
   <div class="header">
     <div class="row">
-      <div class="col-flex form-inputs">
-        <label>Ordenar por</label>
-        <select v-model="settings.orderParam" name="orderParam" @change="refreshContent(settings)">
-          <option value="name">
-            Nombre
-          </option>
-          <option value="count">
-            Contador
-          </option>
-        </select>
-      </div>
-      <div class="col-flex form-inputs">
-        <label>Tipo de orden</label>
-        <select v-model="settings.orderType" name="orderType" @change="refreshContent(settings)">
-          <option value="asc">
-            Ascendente
-          </option>
-          <option value="desc">
-            Descendente
-          </option>
-        </select>
-      </div>
-      <div class="col-flex form-inputs">
-        <label>Rango</label>
-        <input v-model="settings.range" type="text" name="range" @change="refreshContent(settings)">
-      </div>
-      <div class="col-flex form-inputs">
-        <label>Tipo de rango</label>
-        <select v-model="settings.rangeType" name="rangeType" @change="refreshContent(settings)">
-          <option value="mayor">
-            Mayor
-          </option>
-          <option value="menor">
-            Menor
-          </option>
-        </select>
-      </div>
-      <div class="col-flex form-inputs">
-        <button class="blueBtn" @click="cleanSettings()">
-          Limpiar
-        </button>
+      <button class="blueBtn" @click="showFilter()">
+        Filtros
+      </button>
+      <div class="collapse filter">
+        <div class="col-flex form-inputs">
+          <label>Ordenar por</label>
+          <select v-model="settings.orderParam" name="orderParam" @change="refreshContent(settings)">
+            <option value="name">
+              Nombre
+            </option>
+            <option value="count">
+              Contador
+            </option>
+          </select>
+        </div>
+        <div class="col-flex form-inputs">
+          <label>Tipo de orden</label>
+          <select v-model="settings.orderType" name="orderType" @change="refreshContent(settings)">
+            <option value="asc">
+              Ascendente
+            </option>
+            <option value="desc">
+              Descendente
+            </option>
+          </select>
+        </div>
+        <div class="col-flex form-inputs">
+          <label>Rango</label>
+          <input v-model="settings.range" type="text" name="range" @change="refreshContent(settings)">
+        </div>
+        <div class="col-flex form-inputs">
+          <label>Tipo de rango</label>
+          <select v-model="settings.rangeType" name="rangeType" @change="refreshContent(settings)">
+            <option value="mayor">
+              Mayor
+            </option>
+            <option value="menor">
+              Menor
+            </option>
+          </select>
+        </div>
+        <div class="col-flex form-inputs">
+          <button class="blueBtn" @click="cleanSettings()">
+            Limpiar
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -52,7 +57,8 @@ export default {
   data () {
     return {
       settingsForm: {
-      }
+      },
+      statusFilter: true
     }
   },
   computed: {
@@ -69,7 +75,16 @@ export default {
     this.getSettings()
   },
   methods: {
-    ...mapMutations(['changeSettings', 'getSettings', 'refreshContent', 'cleanSettings'])
+    ...mapMutations(['changeSettings', 'getSettings', 'refreshContent', 'cleanSettings']),
+    showFilter () {
+      if (this.statusFilter) {
+        document.getElementsByClassName('collapse filter')[0].style.display = 'none'
+        this.statusFilter = false
+      } else {
+        document.getElementsByClassName('collapse filter')[0].style.display = 'block'
+        this.statusFilter = true
+      }
+    }
   }
 }
 </script>
